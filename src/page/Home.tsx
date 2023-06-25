26bf11cc0b68a62ba0b188e13ab8f8c76733df3a
TechQuery <shiy2008@gmail.com>
2023-06-26 02:20:08 +0800

    [optimize] upgrade Node.js, PNPM, MobX-RESTful table & other Upstream packages

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,17 +1,19 @@
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
+import { ScrollList } from 'mobx-restful-table';
 import { PureComponent } from 'react';
 
 import { AutoMap } from '../component/AutoMap';
 import { FloatIconButton } from '../component/IconButton';
-import { WikiList } from '../component/WikiList';
 import { MapModel } from '../model/Map';
 import { i18n } from '../model/Translation';
-
-const { t } = i18n;
+import { WikiModel } from '../model/Wiki';
+import { WikiCard } from '../component/WikiList';
 
 @observer
 export class HomePage extends PureComponent {
+    store = new WikiModel();
+
     @observable
     keywords = '';
 
@@ -19,7 +21,7 @@ export class HomePage extends PureComponent {
     coordinate?: MapModel['currentCoord'];
 
     render() {
-        const { keywords, coordinate } = this;
+        const { store, keywords, coordinate } = this;
 
         return (
             <main style={{ height: 'calc(100vh - 3.7rem)' }}>
@@ -32,7 +34,18 @@ export class HomePage extends PureComponent {
                 />
                 {keywords && coordinate && (
                     <div className="h-50 overflow-auto px-3 pb-3 bg-white">
-                        <WikiList filter={{ keywords, coordinate }} />
+                        <ScrollList
+                            translator={i18n}
+                            store={store}
+                            filter={{ keywords, coordinate }}
+                            renderList={allItems => (
+                                <ol className="list-unstyled m-0">
+                                    {allItems.map(page => (
+                                        <WikiCard key={page.pageid} {...page} />
+                                    ))}
+                                </ol>
+                            )}
+                        />
                     </div>
                 )}
                 <FloatIconButton name="arrow-clockwise" href="/" />
